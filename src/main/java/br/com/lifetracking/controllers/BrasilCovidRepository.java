e24884a9be187a1875eefbf9b7c0740fd1a49057
Rodrigo Souza <souza.rodrigo61@gmail.com>
2020-06-14 21:06:47 -0300

    fix: pega sempre o registro mais atual

--- src/main/java/br/com/lifetracking/controllers/BrasilCovidRepository.java
+++ src/main/java/br/com/lifetracking/controllers/BrasilCovidRepository.java
@@ -42,7 +42,7 @@ public class BrasilCovidRepository implements PanacheRepository<BrasilCovidEntit
 
         filters.append("and is_last = 'True' ");
         filters.append("and place_type <> 'state' ");
-        filters.append("order by date, state ");
+        filters.append("order by date desc, state asc");
 
         return BrasilCovidEntity.list(filters.toString());
 
